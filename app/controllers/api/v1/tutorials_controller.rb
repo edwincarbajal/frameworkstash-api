b3310dacbe8d88f2eeaa84cf9302ea8920679dd1
Ted-Koomen <koomen.ted@gmail.com>
2017-09-12 22:40:11 -0400

    add like action

--- app/controllers/api/v1/tutorials_controller.rb
+++ app/controllers/api/v1/tutorials_controller.rb
@@ -6,8 +6,23 @@ class Api::V1::TutorialsController < ApplicationController
     render json: tutorials, status: :ok
   end
 
+  def like
+    tutorial = Tutorial.find(params[:id])
+    like = tutorial.likes.build(value: 1, tutorial: tutorial)
+    if like.save
+      head :no_content
+    else
+      render like.errors.fullmessages, status: 403
+    end
+  end
+
+
   private
     def get_framework
       @framework = Framework.find(params[:id])
+    end
+
+    def like_params
+      params.permit(:like).require(:value)
     end
 end
