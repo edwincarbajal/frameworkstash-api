dce44fa0e5975d310d1570a16ebfc552f65cf5c2
Ted-Koomen <koomen.ted@gmail.com>
2017-09-13 17:29:47 -0400

    add subscriptions model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909171011) do
+ActiveRecord::Schema.define(version: 20170913211123) do
 
   create_table "frameworks", force: :cascade do |t|
     t.string "title", null: false
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20170909171011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["tutorial_id"], name: "index_likes_on_tutorial_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tutorials", force: :cascade do |t|
