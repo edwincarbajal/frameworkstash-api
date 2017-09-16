9e2283a28fd041e344d6fcaf662dc5b6cc328535
edwincarbajal <exaviercarbajal@gmail.com>
2017-09-16 19:25:41 -0400

    Update serializer for better json response

--- app/serializers/tutorial_serializer.rb
+++ app/serializers/tutorial_serializer.rb
@@ -1,6 +1,9 @@
 class TutorialSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :author, :url, :skill_level, :created_at, :updated_at, :framework
+  attributes :id, :title, :description, :author, :url, :skill_level, :created_at, :total_likes, :framework
 
   belongs_to :framework
-  has_many :likes
+
+  def total_likes
+    object.likes.count
+  end
 end
