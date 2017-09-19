13e68f4b45513010cf0de1d65c861f541b9122e2
edwincarbajal <exaviercarbajal@gmail.com>
2017-09-18 23:00:56 -0400

    Update migration: Revert to previous v.

--- db/migrate/20170916221928_add_likes_count_to_tutorials.rb
+++ db/migrate/20170916221928_add_likes_count_to_tutorials.rb
@@ -1,18 +1,5 @@
 class AddLikesCountToTutorials < ActiveRecord::Migration[5.1]
   def change
     add_column :tutorials, :likes_count, :integer, default: 0
-
-    reversible do |dir|
-      dir.up { data }
-    end
-  end
-
-  def data
-    execute <<-SQL.squish
-        UPDATE tutorials
-           SET likes_count = (SELECT count(1)
-                                   FROM likes
-                                  WHERE likes.tutorial_id = tutorials.id)
-    SQL
   end
 end
