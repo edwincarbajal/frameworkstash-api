06210536ccea1eb2c45897a7766e255cd0e63dae
edwincarbajal <exaviercarbajal@gmail.com>
2017-09-25 15:03:24 -0400

    Update email subscriptions

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,8 +1,10 @@
 class SubscriptionMailer < ApplicationMailer
   default from: 'frameworkstash@gmail.com'
 
-  def subscription_email
-    @subscritions = Subscription.where(updated_at: 7.days.ago..Time.now)
-    Subscription.all.each{|subscription| mail(to: "#{subscription.email}", subject: "Weekly Update")}
+  def subscription_email(user_email)
+    @tutorials = Tutorial.where(updated_at:
+      7.days.ago..Time.now)
+    @user = user_email.id
+     mail(to: "#{user_email.email}", subject: "Weekly Update")
   end
 end
